1e96e22a09695cd0f406657b99ae38589567f44b
Yulia <konovalova-yuliaa@yandex.ru>
2021-12-02 20:16:42 +0300

    Сделана условная отрисовка кнопок и логирование истории операций

--- vue-calc/src/components/Calculator.vue
+++ vue-calc/src/components/Calculator.vue
@@ -12,16 +12,42 @@
       v-model.lazy.number="operand2"
     />
     <br />
-    <button v-on:click="sum()">+</button>
-    <button v-on:click="minus()">-</button>
-    <button v-on:click="mul()">*</button>
-    <button v-on:click="div()" :disabled="operand2 == 0">/</button>
-    <br />
-    <button v-on:click="stepen()">Степень</button>
-    <button v-on:click="intDiv()">Целочисленное деление</button>
+    <button
+      v-for="(btn, index) in btns"
+      :key="index"
+      @click="calculate(btn)"
+      :disabled="operand1 === '' || operand2 === ''"
+    >
+      {{ btn }}
+    </button>
 
-    <p>Результат:</p>
-    {{ result }}
+    <br />
+
+    <div v-show="error">
+      {{ error }}
+    </div>
+
+    <div v-show="error === ''">
+      <p>Результат:</p>
+      {{ result }}
+    </div>
+
+    <div class="start-message">
+      <template v-if="result < 0">Отрицательное число</template>
+      <template v-else-if="result <= 100">Результат в первой сотне</template>
+      <template v-else>Результат больше 100</template>
+    </div>
+    <div class="list">
+      <div v-for="(item, index) in myColletion" :key="index">
+        {{ item }} - {{ index }}
+      </div>
+    </div>
+    <br />
+    <div class="logsList">
+      <div class="logItem" v-for="(item, id) in logs" :key="id">
+        {{ item }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,26 +63,73 @@ export default {
       result: 0,
       operand1: "",
       operand2: "",
+      error: "",
+      btns: ["+", "-", "*", "/", "^", "int /"],
+      myColletion: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      logs: {},
     };
   },
   methods: {
-    sum() {
-      const { operand1, operand2 } = this;
-      this.result = operand1 + operand2;
+    calculate(operation = "+") {
+      this.error = "";
+      this.result = "";
+      switch (operation) {
+        case "+":
+          this.add();
+          break;
+
+        case "-":
+          this.substract();
+          break;
+
+        case "*":
+          this.multiply();
+          break;
+
+        case "/":
+          this.divide();
+          break;
+
+        case "^":
+          this.exponention();
+          break;
+
+        case "int /":
+          this.intDiv();
+          break;
+      }
+
+      const key = Date.now();
+      const value = `${this.operand1}${operation}${this.operand2} = ${this.result}`;
+      this.$set(this.logs, key, value);
+      // this.logs[ Date.now()] = `${this.operand1}${operation}${this.operand2} = ${this.result}`;
     },
-    minus() {
+    add() {
+      this.result = this.operand1 + this.operand2;
+    },
+    substract() {
       this.result = this.operand1 - this.operand2;
     },
-    mul() {
+    multiply() {
       this.result = this.operand1 * this.operand2;
     },
-    div() {
+    divide() {
+      const { operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "Делить на 0 нельзя";
+        return;
+      }
       this.result = this.operand1 / this.operand2;
     },
-    stepen() {
+    exponention() {
       this.result = this.operand1 ** this.operand2;
     },
     intDiv() {
+      const { operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "Делить на 0 нельзя";
+        return;
+      }
       this.result = parseInt(this.operand1 / this.operand2);
     },
   },
